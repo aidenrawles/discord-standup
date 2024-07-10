8d01daf189f7698e3055520489b49469b5f37e61
Aiden Rawles <aidenrawles27@gmail.com>
2024-07-10 21:38:21 +1000

    fix: include date in shell script

--- send-standup.sh
+++ send-standup.sh
@@ -2,6 +2,7 @@
 
 WEBHOOK_URL=$1
 IMAGE_URL=$2
+DATE=$(date +"%A %d/%m")
 
 curl -H "Content-Type: application/json" \
      -X POST \
@@ -12,7 +13,7 @@ curl -H "Content-Type: application/json" \
        "embeds": [
          {
            "title": "Async Standup",
-           "description": "**Date: Thursday 04/07**\n\n**What did you do since last meeting/standup?**\n[Describe the tasks or goals you accomplished.]\n\n**What are you planning to do today?**\n[Outline the tasks or goals you plan to work on today.]\n\n**Are there any blockers or impediments?**\n[Mention any obstacles that are preventing you from completing your tasks.]\n\n**Additional Notes/Comments:**\n[Any additional information or updates you want to share with the team.]",
+           "description": "**Date: '"$DATE"'**\n\n**What did you do since last meeting/standup?**\n[Describe the tasks or goals you accomplished.]\n\n**What are you planning to do today?**\n[Outline the tasks or goals you plan to work on today.]\n\n**Are there any blockers or impediments?**\n[Mention any obstacles that are preventing you from completing your tasks.]\n\n**Additional Notes/Comments:**\n[Any additional information or updates you want to share with the team.]",
            "color": 5814783
          }
        ]
